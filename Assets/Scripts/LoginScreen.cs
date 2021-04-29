a09f3dd5b56db84c609b5df64e9aeab06b0fb0ec
mpw8hp <michael6022@gmail.com>
2021-04-28 19:24:32 -0500

    Update LoginScreen.cs
    working login script page, working with test login page

--- Assets/Scripts/LoginScreen.cs
+++ Assets/Scripts/LoginScreen.cs
@@ -3,9 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using UnityEngine.Networking;
+using System.Diagnostics;
+
+
 
 public class LoginScreen: MonoBehaviour
 {
+    public string test;
     public Button loginButton;
     public Button registerButton;
     public Button resetButton;
@@ -15,16 +20,41 @@ public class LoginScreen: MonoBehaviour
         loginButton.onClick.AddListener(onLoginButtonPress);
         registerButton.onClick.AddListener(onRegisterButtonPress);
         resetButton.onClick.AddListener(onResetButtonPress);
+        
     }
-
+    IEnumerator GetText()
+    {
+        UnityWebRequest www = UnityWebRequest.Get("https://invisible-plug-game.herokuapp.com/login.php?username=ROC&password=Password");
+        yield return www.SendWebRequest();
+        //if (www.result != UnityWebRequest.Result.Success)
+        //{
+        //Debug.Log(www.error);
+        //}
+        //else
+        //{
+        // Show results as text
+            UnityEngine.Debug.Log(www.downloadHandler.text);
+            //test = www.downloadHandler.text;
+            // Or retrieve results as binary data
+            test = www.downloadHandler.text;
+        UnityEngine.Debug.Log(test);
+        //}
+    }
     // Update is called once per frame
     void Update()
     {
         
     }
 
-    public void onLoginButtonPress() {
-        SceneManager.LoadScene("Menu");
+    public void onLoginButtonPress()
+    {
+        UnityEngine.Debug.Log("Start trying to get to the database");
+        StartCoroutine(GetText());
+        UnityEngine.Debug.Log(test);
+        if(test.Substring(1, 1) == "1"){
+            SceneManager.LoadScene("Menu");
+        }
+        
     }
 
     public void onRegisterButtonPress()
